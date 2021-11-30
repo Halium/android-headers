a3828b51328a0436572503e367d5e6d5dcce073c
Herman van Hazendonk <github.com@herrie.org>
2021-11-30 09:31:28 +0100

    halium-11.0-patched headers

--- hardware/gps.h
+++ hardware/gps.h
@@ -28,6 +28,12 @@
 
 #include "gnss-base.h"
 
+#ifdef __ARM_PCS_VFP
+#define FP_ATTRIB __attribute__((pcs("aapcs")))
+#else
+#define FP_ATTRIB
+#endif
+
 __BEGIN_DECLS
 
 /*
@@ -631,7 +637,7 @@ typedef struct {
      * ID). Latitude and longitude are measured in degrees expected accuracy is
      * measured in meters
      */
-    int  (*inject_location)(double latitude, double longitude, float accuracy);
+    int  (*inject_location)(double latitude, double longitude, float accuracy) FP_ATTRIB;
 
     /**
      * Specifies that the next call to start will not use the
@@ -1200,7 +1206,7 @@ typedef struct {
     */
    void (*add_geofence_area) (int32_t geofence_id, double latitude, double longitude,
        double radius_meters, int last_transition, int monitor_transitions,
-       int notification_responsiveness_ms, int unknown_timer_ms);
+       int notification_responsiveness_ms, int unknown_timer_ms) FP_ATTRIB;
 
    /**
     * Pause monitoring a particular geofence.
